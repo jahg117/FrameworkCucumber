b34953b10bc1c23f54e497cb8a1308b18a8cabe2
kjbj453 <juanalejandro.hernandez@astrazeneca.com>
2020-12-02 11:54:35 -0600

    New cucumber report

--- src/test/java/runner/AbstractTestNGCucumberParallelTests.java
+++ src/test/java/runner/AbstractTestNGCucumberParallelTests.java
@@ -4,10 +4,17 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.FeatureWrapper;
 import io.cucumber.testng.PickleWrapper;
 import io.cucumber.testng.TestNGCucumberRunner;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
+import net.masterthought.cucumber.Reportable;
+import net.masterthought.cucumber.json.support.Status;
+import net.masterthought.cucumber.presentation.PresentationMode;
+import org.testng.annotations.*;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public abstract class AbstractTestNGCucumberParallelTests extends AbstractTestNGCucumberTests {
     private TestNGCucumberRunner testNGCucumberRunner;
@@ -30,5 +37,32 @@ public abstract class AbstractTestNGCucumberParallelTests extends AbstractTestNG
     @AfterClass(alwaysRun = true)
     public void afterClass() {
         testNGCucumberRunner.finish();
+    }
+
+    @AfterSuite
+    public void generateReport() {
+        File reportOutputDirectory = new File("target"); //
+        List<String> jsonFiles = new ArrayList<String>();
+        jsonFiles.add("target/cucumber/cucumber.json");
+
+        String buildNumber = "1";
+        String projectName = "cucumberProject";
+
+        Configuration configuration = new Configuration(reportOutputDirectory, projectName);
+        // do not make scenario failed when step has status SKIPPED
+        configuration.setNotFailingStatuses(Collections.singleton(Status.SKIPPED));
+        configuration.setBuildNumber(buildNumber);
+        // addidtional metadata presented on main page
+        configuration.addClassifications("Platform", "Windows");
+        configuration.addClassifications("Browser", "Chrome");
+        configuration.addClassifications("Branch", "release/1.0");
+
+        // optionally specify qualifiers for each of the report json files
+        configuration.addPresentationModes(PresentationMode.PARALLEL_TESTING);
+        configuration.setQualifier("cucumber-report-1", "First report");
+        configuration.setQualifier("cucumber-report-2", "Second report");
+
+        ReportBuilder reportBuilder = new ReportBuilder(jsonFiles, configuration);
+        Reportable result = reportBuilder.generateReports();
     }
 }
