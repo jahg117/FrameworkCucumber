448b1972105e403460d041d04a980faadfe85ee8
kjbj453 <juanalejandro.hernandez@astrazeneca.com>
2020-12-07 13:35:21 -0600

    ReportRefactor

--- src/test/java/runner/AbstractTestNGCucumberParallelTests.java
+++ src/test/java/runner/AbstractTestNGCucumberParallelTests.java
@@ -4,17 +4,8 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.FeatureWrapper;
 import io.cucumber.testng.PickleWrapper;
 import io.cucumber.testng.TestNGCucumberRunner;
-import net.masterthought.cucumber.Configuration;
-import net.masterthought.cucumber.ReportBuilder;
-import net.masterthought.cucumber.Reportable;
-import net.masterthought.cucumber.json.support.Status;
-import net.masterthought.cucumber.presentation.PresentationMode;
 import org.testng.annotations.*;
-
-import java.io.File;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import utils.CucumberReport;
 
 public abstract class AbstractTestNGCucumberParallelTests extends AbstractTestNGCucumberTests {
     private TestNGCucumberRunner testNGCucumberRunner;
@@ -41,28 +32,6 @@ public abstract class AbstractTestNGCucumberParallelTests extends AbstractTestNG
 
     @AfterSuite
     public void generateReport() {
-        File reportOutputDirectory = new File("target"); //
-        List<String> jsonFiles = new ArrayList<String>();
-        jsonFiles.add("target/cucumber/cucumber.json");
-
-        String buildNumber = "1";
-        String projectName = "cucumberProject";
-
-        Configuration configuration = new Configuration(reportOutputDirectory, projectName);
-        // do not make scenario failed when step has status SKIPPED
-        configuration.setNotFailingStatuses(Collections.singleton(Status.SKIPPED));
-        configuration.setBuildNumber(buildNumber);
-        // addidtional metadata presented on main page
-        configuration.addClassifications("Platform", "Windows");
-        configuration.addClassifications("Browser", "Chrome");
-        configuration.addClassifications("Branch", "release/1.0");
-
-        // optionally specify qualifiers for each of the report json files
-        configuration.addPresentationModes(PresentationMode.PARALLEL_TESTING);
-        configuration.setQualifier("cucumber-report-1", "First report");
-        configuration.setQualifier("cucumber-report-2", "Second report");
-
-        ReportBuilder reportBuilder = new ReportBuilder(jsonFiles, configuration);
-        Reportable result = reportBuilder.generateReports();
+        CucumberReport cucumberReport = new CucumberReport();
     }
 }
