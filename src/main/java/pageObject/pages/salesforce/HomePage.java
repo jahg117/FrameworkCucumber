f5be50847c6162b05ea4d7e3542af659221776a8
kjbj453 <juanalejandro.hernandez@astrazeneca.com>
2021-01-22 12:39:39 -0600

    TC01 Flow complete

--- src/main/java/pageObject/pages/salesforce/HomePage.java
+++ src/main/java/pageObject/pages/salesforce/HomePage.java
@@ -18,10 +18,10 @@ public class HomePage extends CommonFunctions {
     @FindBy(xpath = "//div[contains(@class,'lafPageHost')]")
     private WebElement label_panelTab;
 
-    public void isSalesforcePageVisible() throws InterruptedException {
+    public boolean isSalesforcePageVisible() throws InterruptedException {
         waitForPageToLoad();
         waitForElementVisibility(input_SearchBar, 30);
-        waitForElementAttributeContains(label_panelTab,"data-aura-class","lafPageHost", 30);
+        return waitForElementAttributeContains(label_panelTab,"data-aura-class","lafPageHost", 30);
     }
 
     public void closeOpenTabs() throws Exception {
