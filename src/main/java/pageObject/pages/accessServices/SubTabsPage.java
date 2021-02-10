9c209409164fd783a9bd33a5a4ef663c2c8ad8e3
kjbj453 <juanalejandro.hernandez@astrazeneca.com>
2021-02-10 17:05:26 -0600

    product enrollments

---
+++ src/main/java/pageObject/pages/accessServices/SubTabsPage.java
@@ -0,0 +1,14 @@
+package pageObject.pages.accessServices;
+
+import base.functions.CommonFunctions;
+
+public class SubTabsPage extends CommonFunctions {
+
+    public void switchSubTab(int tab) throws Exception {
+        switchSubTabByIndexSF(tab, 10);
+    }
+
+    public void closeSubTab(int tab) throws Exception {
+        closeSubTabByIndexSF(tab, 10);
+    }
+}
