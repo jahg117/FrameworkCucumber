b8d10d7ef103fb4a4dcc1af407fcadcf837a96e3
kjbj453 <juanalejandro.hernandez@astrazeneca.com>
2021-02-12 12:13:07 -0600

    report update

--- src/test/java/runner/RunCucumberTest.java
+++ src/test/java/runner/RunCucumberTest.java
@@ -9,7 +9,7 @@ import io.cucumber.testng.CucumberOptions;
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
                 "rerun:target/rerun.txt"},
         publish = true,
-        tags = "@regression and @yes")
+        tags = "@smoke")
 
 public class RunCucumberTest extends AbstractTestNGCucumberParallelTests {
 
