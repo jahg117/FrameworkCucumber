a9d5d6a2f8d90459e959baab62f2ee9ab60d2895
kjbj453 <juanalejandro.hernandez@astrazeneca.com>
2021-06-21 12:48:06 -0500

    Create address state

--- src/test/java/stepDefinition/acessServices/CreateAddress.java
+++ src/test/java/stepDefinition/acessServices/CreateAddress.java
@@ -1,20 +1,9 @@
 package stepDefinition.acessServices;
 
-import base.functions.CommonFunctions;
-import io.cucumber.java.en.And;
-import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
-import org.apache.log4j.Logger;
-import org.testng.Assert;
 import pageObject.ApplicationInstance;
-import utils.FileReading;
-import utils.Values;
-import java.util.List;
-import java.util.Locale;
 
 public class CreateAddress extends ApplicationInstance {
-
-    CommonFunctions commonFunctions = new CommonFunctions();
 
     @Then("I add another address with state = MN")
     public void iAddAnotherAddressWithStateMN() throws Exception {
