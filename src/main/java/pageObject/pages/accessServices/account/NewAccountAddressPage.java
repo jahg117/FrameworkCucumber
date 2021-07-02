e782a82d947a80b3f33de231e3fc0b61c7a6a642
Gutiérrez Guzmán, Adolfo <adolfo.gutierrez@astrazeneca.com>
2021-07-02 12:17:59 -0500

    adding more steps to the TC07CreateConsents

--- src/main/java/pageObject/pages/accessServices/account/NewAccountAddressPage.java
+++ src/main/java/pageObject/pages/accessServices/account/NewAccountAddressPage.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import utils.FileReading;
 import utils.Values;
-
 import java.lang.reflect.Method;
 import java.util.List;
 
@@ -15,13 +14,34 @@ public class NewAccountAddressPage extends CommonFunctions {
     @FindBy (xpath = "//*[@placeholder='Search Places']")
     private WebElement googleAddressSearchBox;
 
-    @FindBy (xpath = "//*[@class='slds-listbox__item']")
-    private WebElement firstOpcFromAddressSearchBox;
+    private By firstOpcFromAddressSearchBox = By.xpath("//*[@class='slds-listbox__item']");
+
+    @FindBy (xpath = "(//*[@name='progress'])[last()]")
+    private WebElement addressTypeDropdown;
+
+    @FindBy (xpath = "//*[@data-value='Home']")
+    private WebElement addressTypeHome;
+
+    @FindBy (xpath = "//*[@title='Save']")
+    private WebElement saveButton;
+
 
     public void searchAddressWithStateMN() throws Exception {
-//        googleAddressSearchBox.sendKeys("TCF Bank Stadium");
-        sendKeysElementClickable(googleAddressSearchBox,"TCF Bank Stadium", longWait());
-        clickAndMoveToElementVisible(firstOpcFromAddressSearchBox, longWait());
+        sendKeysElementClickable(googleAddressSearchBox,"TCF Bank Stadium", mediumWait());
+        waitForNumberOfElementsToBeMoreThanBy(firstOpcFromAddressSearchBox, 1, mediumWait());
+        clickAndMoveToElementClickable(getWebElementList(firstOpcFromAddressSearchBox).get(0), mediumWait());
+        clickAndMoveToElementVisible(addressTypeDropdown, mediumWait());
+        clickAndMoveToElementVisible(addressTypeHome, mediumWait());
+        clickAndMoveToElementClickable(saveButton, mediumWait());
+    }
+
+    public void searchAddressWithStateMD() throws Exception {
+        sendKeysElementClickable(googleAddressSearchBox,"Berwyn Heights Elementary School", mediumWait());
+        waitForNumberOfElementsToBeMoreThanBy(firstOpcFromAddressSearchBox, 1, mediumWait());
+        clickAndMoveToElementClickable(getWebElementList(firstOpcFromAddressSearchBox).get(0), mediumWait());
+        clickAndMoveToElementVisible(addressTypeDropdown, mediumWait());
+        clickAndMoveToElementVisible(addressTypeHome, mediumWait());
+        clickAndMoveToElementClickable(saveButton, mediumWait());
     }
 
 }
