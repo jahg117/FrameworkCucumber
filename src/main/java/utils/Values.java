8fd92a0eb4c17d6d688e79148f5cbc504af5603f
Jonathan Ruano <jonathanernesto.ruano@astrazeneca.com>
2021-07-26 13:50:27 -0500

    Add Contact Amy to CC List

--- src/main/java/utils/Values.java
+++ src/main/java/utils/Values.java
@@ -196,7 +196,7 @@ public final class Values {
     public static final String EMAIL_EXTENTPDF = "ExtentPdf.pdf";
 
     //============EMAIL CONTACT LISTS
-    public static final String EMAIL_CCALLLIST = "catherine.goodis2@astrazeneca.com,anum.aziz@astrazeneca.com,alec.wojack@astrazeneca.com,fernando.morales1@astrazeneca.com,monica.cardenas@astrazeneca.com," +
+    public static final String EMAIL_CCALLLIST = "catherine.goodis2@astrazeneca.com,amy.hermann1@astrazeneca.com,anum.aziz@astrazeneca.com,alec.wojack@astrazeneca.com,fernando.morales1@astrazeneca.com,monica.cardenas@astrazeneca.com," +
             "alixdria.carreon@astrazeneca.com,adriana.ramirez@astrazeneca.com,laura.burgueno@astrazeneca.com,harsh.marwaha@astrazeneca.com,Timothy.Tyler@astrazeneca.com,Peggy.Metzger@astrazeneca.com," +
             "salvadorantonio.hernandez@astrazeneca.com,ana.garcia3@astrazeneca.com,adolfo.gutierrez@astrazeneca.com,hector.urena@astrazeneca.com,juan.marquez1@astrazeneca.com,pedro.flores@astrazeneca.com," +
             "viridiana.salinas@astrazeneca.com,alejandra.guiot@astrazeneca.com,cesar.magana@astrazeneca.com,isselsinai.ramirez@astrazeneca.com,roberto.campos@astrazeneca.com,daniela.salazar2@astrazeneca.com";
