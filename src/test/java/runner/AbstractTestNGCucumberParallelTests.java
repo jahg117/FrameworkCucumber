18a8fae4b718e748b4608c675a4c9c7734f138c8
Jonathan Ruano <jonathanernesto.ruano@astrazeneca.com>
2021-07-26 13:29:50 -0500

    Add Contact Amy to CC List

--- src/test/java/runner/AbstractTestNGCucumberParallelTests.java
+++ src/test/java/runner/AbstractTestNGCucumberParallelTests.java
@@ -11,7 +11,6 @@ import utils.FileReading;
 import utils.SendEmail;
 import utils.Values;
 
-import javax.mail.MessagingException;
 
 public abstract class AbstractTestNGCucumberParallelTests extends AbstractTestNGCucumberTests {
     int reportCounter = 0;
@@ -52,7 +51,7 @@ public abstract class AbstractTestNGCucumberParallelTests extends AbstractTestNG
         CucumberReport cucumberReport = new CucumberReport();
         fileReading.setFileName(Values.TXT_GLOBAL_PROPERTIES);
         if (commonFunctions.searchAFile(Values.EMAIL_PATHPDF, Values.EMAIL_EXTENTPDF) &&
-                fileReading.getField("sendReport").trim().equalsIgnoreCase(Values.TXT_VALTRUE) && reportCounter==0) {
+                fileReading.getField("sendReport").trim().equalsIgnoreCase(Values.TXT_VALTRUE) && reportCounter == 0) {
             sendEmail.emailAttachment(Values.EMAIL_PATHPDF, Values.EMAIL_EXTENTPDF);
             reportCounter++;
         }
